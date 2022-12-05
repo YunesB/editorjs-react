60824cb70e9334fba3f28db95ff559fffc454b0f
Yunes B <y.bakhush@petrel.ai>
2022-12-05 20:35:23 +0600

    fix: logic fixed

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,15 +1,46 @@
+import { FC, useMemo, useState } from 'react';
+
 import { Parser } from '@alkhipce/editorjs-react';
 import { IParser } from '@alkhipce/editorjs-react/dist/types/ParserData';
-import { FC, useState } from 'react';
-import { mockData } from '../../utils/mockData';
-import { Form } from '../Form';
-import './App.css';
 
-const parseData = (data: string) => JSON.parse(data);
+import { mockData } from '../../utils/mockData';
+
+import { Form } from '../Form';
+import logo from '../../assets/editorjs_logo.svg';
+
+const HEADER_LINKS = [
+  {
+    name: 'Editor JS',
+    link: 'https://editorjs.io/'
+  },
+  {
+    name: 'NPM',
+    link: 'https://www.npmjs.com/package/@alkhipce/editorjs-react'
+  },
+  {
+    name: 'GitHub',
+    link: 'https://github.com/etozhealkhipce/editorjs-react'
+  }
+];
 
 const App: FC = () => {
-  const initialData = parseData(mockData);
+  const [isError, setError] = useState(false);
 
+  const parseData = (data: string) => {
+    try {
+      setError(false);
+      return JSON.parse(data);
+    } catch (err) {
+      setError(true);
+      return {
+        time: 0,
+        version: '',
+        blocks: []
+      };
+    }
+  };
+
+  const initialData = useMemo(() => parseData(mockData), []);
   const [parsedData, setParsedData] = useState<IParser>(initialData);
 
   const handleSubmit = (data: string) => {
@@ -17,17 +48,39 @@ const App: FC = () => {
   };
 
   return (
-    <div className="content">
-      <header className="header">Header</header>
+    <div className="page">
+      <header className="header">
+        <div className="header-container">
+          <div className="header__logo-container">
+            <img src={logo} alt="logo" className="header__logo" />
+            <p className="header__title">@alkhipce/editorjs-react</p>
+          </div>
+          <ul className="header__list">
+            {HEADER_LINKS.map((hl) => (
+              <li className="header__list-item" key={hl.name}>
+                <a href={hl.link} target="_blank" rel="noreferrer">
+                  {hl.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </header>
 
-      <div className="centered_block">
-        <Form handleSubmit={handleSubmit} />
-      </div>
+      <div className="content">
+        <div className="centered_block">
+          <Form handleSubmit={handleSubmit} />
+        </div>
 
-      <div className="centered_block">
-        <Parser data={parsedData} />
+        <div className="centered_block">
+          <Parser data={parsedData} />
+          {isError && (
+            <span className="error_message">
+              Invalid JSON, please try again.
+            </span>
+          )}
+        </div>
       </div>
-      <footer>Footer</footer>
     </div>
   );
 };
--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,31 +1,36 @@
 import React, { FC, useState } from 'react';
-import { mockData } from '../../utils/mockData';
 import './index.css';
+
+import Editor from 'react-simple-code-editor';
+
+import { highlight, languages } from 'prismjs';
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/themes/prism.css';
+
+import { mockData } from '../../utils/mockData';
 
 type TProps = {
   handleSubmit: (v: string) => void;
 };
 
-const INPUT_INDEX = 0;
-
 export const Form: FC<TProps> = ({ handleSubmit }) => {
-  const [inputValue, setInputValue] = useState('');
-
-  console.log(inputValue);
+  const [code, setCode] = useState(mockData);
 
   const onSubit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // @ts-ignore
-    handleSubmit(e.target[INPUT_INDEX].value);
+    handleSubmit(code);
   };
 
   return (
     <form onSubmit={(e) => onSubit(e)} className="form">
-      <textarea
-        name="input"
-        className="form__input"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
+      <h1 className="form__title">Please paste your JSON:</h1>
+      <Editor
+        value={code}
+        onValueChange={(code) => setCode(code)}
+        highlight={(code) => highlight(code, languages.js, 'js')}
+        className="form__editor"
+        padding={10}
       />
       <button type="submit" className="form__button">
         Submit
